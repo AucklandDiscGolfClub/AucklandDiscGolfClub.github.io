a568d3ed0b9d9e3197a1b9e5c1211091620407b3
Ryan Smale <eagle14@gmail.com>
2022-07-08 01:14:54 +1200

    tweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}","./pages/**/*.{html,js}",
-  './index.html','./404.html'],
+  './index.html','./404.html','./node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ]
 }
+
