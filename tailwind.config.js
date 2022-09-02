794dcfb1be026f38d5409f311aa982911bc3ad1e
Ryan Smale <eagle14@gmail.com>
2022-09-03 02:11:05 +1200

    updated events

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}","./pages/**/*.{html,js}",
   './index.html','./404.html','./node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'natural-green': '#57716c',
+        'lighter-green': '#89aaae',
+        'warning-coral': '#d77677',
+        'ADGC-logo-blue': '#0077b2',
+      },
+    },
   },
   plugins: [
     require('tw-elements/dist/plugin')
