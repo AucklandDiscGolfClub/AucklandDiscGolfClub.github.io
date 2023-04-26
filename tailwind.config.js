f6a6491dbb2bcd172797a74ad9e7ddbed9dcb5f2
Ryan Smale <eagle14@gmail.com>
2023-04-26 19:36:06 +1200

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,32 @@ module.exports = {
         'warning-coral': '#d77677',
         'ADGC-logo-blue': '#0077b2',
       },
+      'animation': {
+          'text':'text 5s ease infinite',
+          'backgroundd' : 'backy 5s ease infinite'
+      },
+      'keyframes': {
+          'text': {
+              '0%, 100%': {
+                 'background-size':'200% 200%',
+                  'background-position': 'left center'
+              },
+              '50%': {
+                 'background-size':'200% 200%',
+                  'background-position': 'right center'
+              }
+          },
+          'backy': {
+              '0%, 100%': {
+                 'background-size':'200% 200%',
+                  'background-position': 'top center'
+              },
+              '50%': {
+                 'background-size':'200% 200%',
+                  'background-position': 'bottom center'
+              }
+          },
+      },
     },
   },
   plugins: [
